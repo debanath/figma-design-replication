c57dea0d53a28ec06e4eb5719aaacbbec0c63e55
Debashish Nath <57856461+debanath@users.noreply.github.com>
2024-06-29 07:49:44 +0530

    feat: add booking form

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-const onClick = () => {
-    alert("Hello World");
+var modal = document.getElementById("bookForm");
+var btn1 = document.getElementById("bookBtn");
+var btn2 = document.getElementById("startBtn");
+var span1 = document.getElementsByClassName("form-close");
+
+btn1.onclick = function () {
+    modal.style.display = "flex";
+};
+
+btn2.onclick = function () {
+    modal.style.display = "flex";
+};
+
+span1.onclick = function () {
+    modal.style.display = "none";
+};
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 };
